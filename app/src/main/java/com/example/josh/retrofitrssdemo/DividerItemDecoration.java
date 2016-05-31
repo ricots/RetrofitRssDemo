e5c288fb1fe2a6545ea948a129150cc965dc807e
Josh-Skrzypczak <jskrzypczak@house.mi.gov>
2016-05-30 23:32:00 -0400

    FABBehavior and ItemDecoration

---
+++ app/src/main/java/com/example/josh/retrofitrssdemo/DividerItemDecoration.java
@@ -0,0 +1,41 @@
+package com.example.josh.retrofitrssdemo;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by Josh on 5/18/2016.
+ */
+public class DividerItemDecoration extends RecyclerView.ItemDecoration{
+
+    private Drawable mDivider;
+
+    public DividerItemDecoration(Context context) {
+        //mDivider = ContextCompat.getDrawable(context,R.drawable.divider);
+        mDivider = context.getResources().getDrawable(R.drawable.divider);
+    }
+
+    @Override
+    public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        int left = parent.getPaddingLeft();
+        int right = parent.getWidth() - parent.getPaddingRight();
+
+        int childCount = parent.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View child = parent.getChildAt(i);
+
+            RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+
+            int top = child.getBottom() + params.bottomMargin;
+            int bottom = top + mDivider.getIntrinsicHeight();
+
+            mDivider.setBounds(left, top, right, bottom);
+            mDivider.draw(c);
+        }
+    }
+
+
+}
